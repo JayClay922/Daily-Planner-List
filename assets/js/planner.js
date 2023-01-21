f468d0063f1d541e8d2fddc47a26c9b3719813e5
JayClay922 <jamesclayman922@live.com>
2023-01-21 11:07:10 +0000

    adding comments to my first blocks of code

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,3 +1,5 @@
+//Using moment to set the the date in the format below and using jQuery to center the text
 let today = moment();
 currentDate = $("#currentDay").text(today.format("dddd, MMMM Do"));
 $(".jumbotron").css("text-align", "center");
+
