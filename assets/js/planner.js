926e99460a55376ff9fc6204c9e99dc4f1a6f797
JayClay922 <jamesclayman922@live.com>
2023-01-21 19:57:14 +0000

    adding timeblocks to HTML

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -4,11 +4,3 @@ currentDate = $("#currentDay").text(today.format("dddd, MMMM Do"));
 $(".jumbotron").css("text-align", "center");
 
 
-//Adding timeblocks in rows
-for (let i = 9; i <= 17; i++) {
-    let rowDiv = $("<div>").addClass("row");
-    let hourDiv = $("<div>").addClass("time-block hour").text(i + ":00");
-    let textArea = $("<textarea>").addClass("textarea description");
-    rowDiv.append(hourDiv, textArea);
-    $(".container").append(rowDiv);
-};
