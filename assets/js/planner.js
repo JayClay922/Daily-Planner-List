ce2b09935b09f7ed77d5dc9ef8aab75ee46c2d05
JayClay922 <jamesclayman922@live.com>
2023-01-23 09:03:12 +0000

    updating code for clicking save button

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -3,4 +3,15 @@ let today = moment();
 currentDate = $("#currentDay").text(today.format("dddd, MMMM Do"));
 $(".jumbotron").css("text-align", "center");
 
+$(document).ready(function() {
+    $("#saveBtn").click(function(event) {
+        var text = $(".textarea .description").val();
+        JSON.parse(localStorage.setItem("savedText", text))
+    });
+
+    var savedText = JSON.stringify(localStorage.getItem("savedText"));
+    if (savedText) {
+        $(".textarea .description").val(savedText);
+    }
+});
 
