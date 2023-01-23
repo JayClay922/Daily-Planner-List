ea05880d4dabd69a78c1e2099d14b5421bab0d74
JayClay922 <jamesclayman922@live.com>
2023-01-23 10:24:57 +0000

    adding comments to my js file

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,9 +1,9 @@
-//Using moment to set the the date in the format below and using jQuery to center the text
+//Using moment to set the the date in the format below and using jQuery to center the text.
 let today = moment();
 currentDate = $("#currentDay").text(today.format("dddd, MMMM Do"));
 $(".jumbotron").css("text-align", "center");
 
-
+// Adding event listener on the save button so that when it is clicked the values are saved on the page.
 $(".saveBtn").on("click", function(){
     let value = $(this)
     .siblings(".description")
@@ -16,6 +16,7 @@ $(".saveBtn").on("click", function(){
     localStorage.setItem(time, value)
 });
 
+// Getting the values from local storage, and displaying them in their respective timeslots in the DOM"
 $('#0900 .description').val(localStorage.getItem('0900'));
 $('#1000 .description').val(localStorage.getItem('1000'));
 $('#1100 .description').val(localStorage.getItem('1100'));
